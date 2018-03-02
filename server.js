b3fba69d2190c9e0e4dc1c6b345e4c77e7d3e089
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-01 21:12:47 -0800

    updated port settings

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -19,4 +19,4 @@ var routes = require("./controllers/milkshake_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT);
