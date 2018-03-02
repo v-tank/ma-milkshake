426bdc9a700a2493535f3be4eac9fc1a21c3cfae
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-01 22:14:26 -0800

    added comments

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
+// Require the mysql NPM package
 var mysql = require("mysql");
 
 var connection;
 
+// Choose which settings to use depending on the environment (local vs. heroku)
 if (process.env.JAWSDB_URL) { 
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -14,6 +16,7 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
+// Create connection
 connection.connect(function(err) {
   if (err) {
     console.log("Error connecting: " + err.stack);
@@ -22,4 +25,4 @@ connection.connect(function(err) {
   console.log("Connected as ID " + connection.threadId);
 });
 
-module.exports = connection;
+module.exports = connection; // Export the connection to be used by the ORM
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 -- CREATE DATABASE milkshake_db;
-USE nnxor34mj5tykc7h;
+USE nnxor34mj5tykc7h; -- database created by jawsDB 
 
+-- Creates the table
 CREATE TABLE milkshakes (
   id INT NOT NULL AUTO_INCREMENT,
   milkshake_name VARCHAR(100) NOT NULL,
