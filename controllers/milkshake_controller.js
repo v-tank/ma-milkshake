560ae1a2c99f16c3584586be8e51a21972e89698
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-01 20:27:25 -0800

    added update route

--- controllers/milkshake_controller.js
+++ controllers/milkshake_controller.js
@@ -13,9 +13,24 @@ router.get("/", function(req, res) {
 });
 
 router.post("/milkshakes", function(req, res) {
-  milkshake.insertOne(["milkshake_name"],[req.body.milkshake_name], function(result) {
+  milkshake.insertOne(["milkshake_name"],[req.body.name], function(result) {
     res.json( {id: result.id} );
   });
 });
+
+router.put("/milkshakes/:id", function(req, res) {
+  var whereCondition = "id = " + req.params.id;
+  // console.log(whereCondition);
+
+  milkshake.updateOne({
+    gulped: req.body.gulped
+  }, whereCondition, function(result) {
+    if (res.changedRows === 0) {
+      return res.status(404).end();
+    } else {
+      return res.status(200).end();
+    }
+  })
+})
 
 module.exports = router;
